4809644ba450a7296e25b5d4229c3d6bc56d3af2
Kat-Jester <hortonhandlesit@gmail.com>
2025-02-14 13:34:21 -0500

    Final SkillBuider 3

--- src/main/java/SkillBuilder3.java
+++ src/main/java/SkillBuilder3.java
@@ -11,43 +11,68 @@ public class SkillBuilder3
     private static String repeat(String s, int numOfTimesToRepeat)
     {
         String result = "";
-        // TODO: replace this line with your code
+        for (int i = 0; i < numOfTimesToRepeat; i++) {
+            result += s;
+        }
         return result;
     }
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                str += "*";
+            }
+            str += "\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                str += " ";
+            }
+            for (int j = 1; j <= i; j++) {
+                str += "*";
+            }
+            str += "\n";
+        }
         return str;
     }
 
-    public static String circle(int radius)
-    {
+    public static String circle(int radius){
         String str = "";
-        // TODO: replace this line with your code
+
+        for (int y = radius - 1; y >= -radius + 1; y--) {
+            int x = (int) Math.sqrt(radius * radius - y * y);
+            int spaces = radius - x;
+            int stars = 2 * x;
+
+            str += " ".repeat(spaces) + "*".repeat(stars) + "\n";
+        }
+
         return str;
     }
 
     public static long sumOfDivisors(long number) {
         long sum = 0;
-        // TODO: replace this line with your code
+        for (long i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
         return sum;
     }
 
     public static boolean isPerfect(long number){
-        return false;
+        return sumOfDivisors(number) == number && number != 0;
     }
 
     public static long sumOfDivisorsUsingWhile(long number){
         long sum = 0;
-        // TODO: replace this line with your code
         return sum;
     }
 }
