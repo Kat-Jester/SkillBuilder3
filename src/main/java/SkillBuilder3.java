fc156a875fc1d3689eaab8b5503e38d529ff120a
Kat-Jester <hortonhandlesit@gmail.com>
2025-02-14 13:49:15 -0500

    Final Fixed Skill Builder Using Proper repeat builder and Sum of divsor While loop completed

--- src/main/java/SkillBuilder3.java
+++ src/main/java/SkillBuilder3.java
@@ -6,56 +6,45 @@
  * @version 1.0
  */
 
-public class SkillBuilder3
-{
-    private static String repeat(String s, int numOfTimesToRepeat)
-    {
+public class SkillBuilder3 {
+    private static String repeat(String s, int numOfTimesToRepeat) {
         String result = "";
         for (int i = 0; i < numOfTimesToRepeat; i++) {
             result += s;
         }
         return result;
     }
-    public static String leftRightTriangle(int height)
-    {
+
+    public static String leftRightTriangle(int height) {
         String str = "";
         for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= i; j++) {
-                str += "*";
-            }
-            str += "\n";
+            str += "*".repeat(i) + "\n";
         }
         return str;
     }
 
-    public static String rightRightTriangle(int height)
-    {
+    public static String rightRightTriangle(int height) {
         String str = "";
         for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= height - i; j++) {
-                str += " ";
-            }
-            for (int j = 1; j <= i; j++) {
-                str += "*";
-            }
-            str += "\n";
+            str += " ".repeat(height - i) + "*".repeat(i) + "\n";
         }
         return str;
     }
 
-    public static String circle(int radius){
-        String str = "";
+    public static String circle(int radius) {
+        StringBuilder str = new StringBuilder();
 
         for (int y = radius - 1; y >= -radius + 1; y--) {
             int x = (int) Math.sqrt(radius * radius - y * y);
             int spaces = radius - x;
             int stars = 2 * x;
 
-            str += " ".repeat(spaces) + "*".repeat(stars) + "\n";
+            str.append(" ".repeat(spaces)).append("*".repeat(stars)).append("\n");
         }
 
-        return str;
+        return str.toString();
     }
+
 
     public static long sumOfDivisors(long number) {
         long sum = 0;
@@ -67,12 +56,19 @@ public class SkillBuilder3
         return sum;
     }
 
-    public static boolean isPerfect(long number){
+    public static boolean isPerfect(long number) {
         return sumOfDivisors(number) == number && number != 0;
     }
 
-    public static long sumOfDivisorsUsingWhile(long number){
+    public static long sumOfDivisorsUsingWhile(long number) {
         long sum = 0;
+        long i = 1;
+        while (i <= number / 2) {
+            if (number % i == 0) {
+                sum += i;
+            }
+            i++;
+        }
         return sum;
     }
 }
